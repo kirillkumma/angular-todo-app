d5cff3ffab8e4edc92a9ef03f92c1bdffe2b33f3
Kirill Kumma <kirill.kumma@gmail.com>
2022-02-11 00:32:17 +0300

    modules: connect login with auth logic

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../services/auth.service';
 
 @Component({ selector: 'app-login', templateUrl: './login.component.html' })
 export class LoginComponent {
+  error?: string;
   form = new FormGroup({
     email: new FormControl(''),
     password: new FormControl(''),
   });
+
+  constructor(private _authService: AuthService, private _router: Router) {
+    this._authService.state.subscribe(({ user }) => {
+      if (user) {
+        this._router.navigateByUrl('/');
+      }
+    });
+  }
 
   isControlInvalid(name: string) {
     return this.form.controls[name].invalid && this.form.controls[name].touched;
@@ -18,7 +30,11 @@ export class LoginComponent {
 
   onSubmit() {
     if (this.form.valid) {
-      console.log(this.form.value);
+      this._authService.login(this.form.value).subscribe(({ error }) => {
+        if (error) {
+          this.error = error.message;
+        }
+      });
     }
   }
 }
