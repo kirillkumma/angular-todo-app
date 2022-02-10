c0dbb5e58c91be6672ed4113699f1a4649ff9b18
Kirill Kumma <kirill.kumma@gmail.com>
2022-02-11 00:31:40 +0300

    modules: add HttpClientModule, AuthService and http interceptors to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { AuthService } from './services/auth.service';
+import { httpInterceptros } from './http/';
 
 @NgModule({
   declarations: [AppComponent, RegisterComponent, LoginComponent],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
+  providers: [AuthService, httpInterceptros],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
