7f5ffa0be42f58d863a3f6328ad90ad1001d60fd
Kirill Kumma <kirill.kumma@gmail.com>
2022-02-11 00:34:22 +0300

    modules: connect register with auth logic

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,16 +1,28 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
 })
 export class RegisterComponent {
+  error?: string;
   form = new FormGroup({
     email: new FormControl(''),
     name: new FormControl(''),
     password: new FormControl(''),
   });
+
+  constructor(private _authService: AuthService, private _router: Router) {
+    this._authService.state.subscribe(({ user }) => {
+      if (user) {
+        this._router.navigateByUrl('/');
+      }
+    });
+  }
 
   isControlInvalid(name: string) {
     return this.form.controls[name].invalid && this.form.controls[name].touched;
@@ -22,7 +34,11 @@ export class RegisterComponent {
 
   onSubmit() {
     if (this.form.valid) {
-      console.log(this.form.value);
+      this._authService.register(this.form.value).subscribe(({ error }) => {
+        if (error) {
+          this.error = error.message;
+        }
+      });
     }
   }
 }
