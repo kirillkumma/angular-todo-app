eee548d54b3e4de023c2ef6bb23116419fe7b01e
Kirill Kumma <kirill.kumma@gmail.com>
2022-02-10 22:21:14 +0300

    modules: add form logic to login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,24 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({ selector: 'app-login', templateUrl: './login.component.html' })
-export class LoginComponent {}
+export class LoginComponent {
+  form = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  isControlInvalid(name: string) {
+    return this.form.controls[name].invalid && this.form.controls[name].touched;
+  }
+
+  getControlError(controlName: string, errorName: string) {
+    return this.form.controls[controlName].errors?.[errorName];
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      console.log(this.form.value);
+    }
+  }
+}
