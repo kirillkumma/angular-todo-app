11f9269ff8188a1e3f3201953db1ae1af05095cd
Kirill Kumma <kirill.kumma@gmail.com>
2022-02-10 22:21:00 +0300

    modules: add form logic to register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,7 +1,28 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  form = new FormGroup({
+    email: new FormControl(''),
+    name: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  isControlInvalid(name: string) {
+    return this.form.controls[name].invalid && this.form.controls[name].touched;
+  }
+
+  getControlError(controlName: string, errorName: string) {
+    return this.form.controls[controlName].errors?.[errorName];
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      console.log(this.form.value);
+    }
+  }
+}
