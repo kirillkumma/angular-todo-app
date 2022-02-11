80850ffb5a4dcdcabad72703fbbd56e83acdab57
Kirill Kumma <kirill.kumma@gmail.com>
2022-02-12 00:56:12 +0300

    modules: call authenticate method on app init and redirect on auth state change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject, tap } from 'rxjs';
+
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  isLoading = new BehaviorSubject(true);
+
+  constructor(private _authService: AuthService, private _router: Router) {
+    this._authService.state.subscribe(({ user }) => {
+      if (user) {
+        this._router.navigateByUrl('/');
+      } else {
+        this._router.navigateByUrl('/login');
+      }
+    });
+  }
+
+  ngOnInit() {
+    this._authService.authenticate().subscribe(() => {
+      this.isLoading.next(false);
+    });
+  }
+}
