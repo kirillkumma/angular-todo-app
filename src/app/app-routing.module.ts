c622c8a828a38738ecc3f7801151a932ac204235
Kirill Kumma <kirill.kumma@gmail.com>
2022-02-12 02:58:28 +0300

    routes: add todo list view as a home route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { TodoListComponent } from './todos/todo-list.component';
 
 const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
+  { path: '', component: TodoListComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { TodoListComponent } from './todos/todo-list.component';
+import { TodoComponent } from './todos/todo.component';
 import { AuthService } from './services/auth.service';
+import { TodoService } from './services/todo.service';
 import { httpInterceptros } from './http/';
 
 @NgModule({
-  declarations: [AppComponent, RegisterComponent, LoginComponent],
+  declarations: [
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    TodoListComponent,
+    TodoComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
   ],
-  providers: [AuthService, httpInterceptros],
+  providers: [AuthService, httpInterceptros, TodoService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
