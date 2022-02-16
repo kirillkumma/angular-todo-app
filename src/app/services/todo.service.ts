4958f79c8a89dd00f1168eb3d9fc0ac01a7974dd
Kirill Kumma <kirill.kumma@gmail.com>
2022-02-17 01:09:13 +0300

    services: add rest of crud operations to todo

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -12,8 +12,16 @@ interface FetchTodosResponse {
   data: Todo[];
 }
 
+interface CreateTodoResponse {
+  data: Todo;
+}
+
 interface UpdateTodoResponse {
   data: Todo;
+}
+
+interface DeleteTodoResponse {
+  data: number;
 }
 
 @Injectable({ providedIn: 'root' })
@@ -31,26 +39,43 @@ export class TodoService {
     });
   }
 
+  createTodo(newTodo: { title: string }) {
+    this._http
+      .post<CreateTodoResponse>('/todos', newTodo, {
+        headers: { 'content-type': 'application/json' },
+      })
+      .subscribe(({ data }) => {
+        this.todos.next([...this.todos.getValue(), data]);
+      });
+  }
+
   updateTodo({
     id,
-    title,
-    completed,
+    ...updatedTodo
   }: {
     id: number;
     title?: string;
     completed?: boolean;
   }) {
     this._http
-      .put<UpdateTodoResponse>(
-        `/todos/${id}`,
-        { title, completed },
-        { headers: { 'content-type': 'application/json' } },
-      )
+      .put<UpdateTodoResponse>(`/todos/${id}`, updatedTodo, {
+        headers: { 'content-type': 'application/json' },
+      })
       .subscribe(({ data }) => {
         this.todos.next(
           this.todos
             .getValue()
             .map((todo) => (todo.id === data.id ? data : todo)),
+        );
+      });
+  }
+
+  deleteTodo(id: number) {
+    this._http
+      .delete<DeleteTodoResponse>(`/todos/${id}`)
+      .subscribe(({ data }) => {
+        this.todos.next(
+          this.todos.getValue().filter((todo) => todo.id !== data),
         );
       });
   }
