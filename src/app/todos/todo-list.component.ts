7368a1aedf510809a3e4b90ef6cb4fb7c3dadfae
Kirill Kumma <kirill.kumma@gmail.com>
2022-02-17 01:11:09 +0300

    modules: implement all crud operations in ui of todos

--- src/app/todos/todo-list.component.ts
+++ src/app/todos/todo-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 import { TodoService, Todo } from '../services/todo.service';
 
@@ -7,31 +9,48 @@ import { TodoService, Todo } from '../services/todo.service';
   templateUrl: './todo-list.component.html',
 })
 export class TodoListComponent implements OnInit {
+  form = new FormGroup({
+    title: new FormControl(''),
+  });
   todos: Todo[] = [];
   isLoading = false;
 
-  constructor(private _todoService: TodoService) {
+  constructor(private _todoService: TodoService) {}
+
+  ngOnInit() {
     this._todoService.todos.subscribe((todos) => {
       this.todos = todos;
     });
-    this._todoService.isLoading.subscribe((isLoading) => {
-      this.isLoading = isLoading;
-    });
-  }
 
-  ngOnInit() {
+    this._todoService.isLoading
+      .pipe(debounceTime(1000))
+      .subscribe((isLoading) => {
+        this.isLoading = isLoading;
+      });
+
     this._todoService.fetchTodos();
   }
 
-  onCompletedChange({
-    id,
-    completed,
-    title,
-  }: {
-    id: number;
-    completed: boolean;
-    title: string;
-  }) {
-    this._todoService.updateTodo({ id, completed, title });
+  isControlInvalid(name: string) {
+    return this.form.controls[name].invalid && this.form.controls[name].touched;
+  }
+
+  getControlError(controlName: string, errorName: string) {
+    return this.form.controls[controlName].errors?.[errorName];
+  }
+
+  onCreateTodo() {
+    if (this.form.valid) {
+      this._todoService.createTodo({ title: this.form.value.title.trim() });
+      this.form.reset();
+    }
+  }
+
+  onChange(updatedTodo: { id: number; completed: boolean; title: string }) {
+    this._todoService.updateTodo(updatedTodo);
+  }
+
+  onDelete(id: number) {
+    this._todoService.deleteTodo(id);
   }
 }
--- src/app/todos/todo.component.ts
+++ src/app/todos/todo.component.ts
@@ -1,4 +1,15 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnInit,
+  AfterViewInit,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 import { Todo } from '../services/todo.service';
 
@@ -6,19 +17,46 @@ import { Todo } from '../services/todo.service';
   selector: 'app-todo',
   templateUrl: './todo.component.html',
 })
-export class TodoComponent {
+export class TodoComponent implements OnInit, AfterViewInit {
   @Input() todo!: Todo;
-  @Output() onCompletedChange: EventEmitter<{
+  @Output() onChange: EventEmitter<{
     id: number;
     completed: boolean;
     title: string;
   }> = new EventEmitter();
+  @Output() onDelete: EventEmitter<number> = new EventEmitter();
+  titleControl!: FormControl;
+  @ViewChild('title') titleEl!: ElementRef<HTMLTextAreaElement>;
 
-  onClick(e: any) {
-    this.onCompletedChange.emit({
-      id: this.todo.id,
-      completed: e.target.checked,
-      title: this.todo.title,
+  ngOnInit() {
+    this.titleControl = new FormControl(this.todo.title);
+    this.titleControl.valueChanges
+      .pipe(debounceTime(1000))
+      .subscribe((title) => {
+        this.onChange.emit({ ...this.todo, title: title.trim() });
+      });
+  }
+
+  ngAfterViewInit() {
+    const el = this.titleEl.nativeElement;
+    el.style.height = '1px';
+    el.style.height = `${el.scrollHeight}px`;
+  }
+
+  onKeyUp() {
+    const el = this.titleEl.nativeElement;
+    el.style.height = '1px';
+    setTimeout(() => (el.style.height = `${el.scrollHeight}px`), 1);
+  }
+
+  onCheckboxClick() {
+    this.onChange.emit({
+      ...this.todo,
+      completed: !this.todo.completed,
     });
+  }
+
+  onButtonClick() {
+    this.onDelete.emit(this.todo.id);
   }
 }
